8b8256abf3362a9b3c0f21602fb308120753db78
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-03-24 21:33:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Countries from './components/countries/Countries'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <h3>Introduction to React</h3>
+    <Countries></Countries>
+    </>
+  )
+}
+
+export default App
