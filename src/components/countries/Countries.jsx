5ac2b49cd57dc7b370545557b348389af0cf61e6
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-03-25 01:37:09 +0600

    3rd commit

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { use } from "react";
 import Country from './Country';
 
 const Countries = ({something}) => {
     const whatever = use(something);
     const countries = whatever.countries;
+
+
+
+    const [vcountries, setVcountries] = useState([]);
+    
+    const handleVC = (country) =>{
+    const nvcountries = [...vcountries, country];
+
+       setVcountries(nvcountries);
+    }
+
     // console.log(whatever.countries);
     return (
         <div>
             <h3>Country roads take me home</h3>
+            <h3>Total Countries: {countries.length}</h3>
+            <ol>
+                {
+                    vcountries.map(vcountry => <li>{vcountry.name.common}</li>)
+                }
+            </ol>
+            <h2>visited countries: {vcountries.length}</h2>
              <div className='countryCon'>
             {
-                countries.map(country => <Country country={country}></Country>)
+                countries.map(country => <Country country={country} handleVC={handleVC}></Country>)
             }
             </div>
         </div>
--- src/components/countries/Country.jsx
+++ src/components/countries/Country.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Country = ({country}) => {
+
+
+
+const Country = ({country, handleVC}) => {
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+    // console.log('clicked');
+     setVisited(!visited);
+     handleVC(country);
+    
+}
 
     // const {name, currency} = country.name.common, country.capital.capital;
     return (
       
-        <div className='countrySty'>
+        <div className={`countrySty ${visited && 'visited'}`}>
             <h1>Welcome</h1>
             <h3>Name: {country.name.common}</h3>
             <h3>Flag: {}</h3>
             <img src={country.flags.flags.png} alt="" />
+            <button className='btn' onClick={handleVisited} >{visited ? 'Visited' : 'Not Visited'}</button>
         </div>
     );
 };
